2e663070baee40889ecc5010eb6877849c4cf8e6
Miguel Monteiro Santos <miguelmonteirosantos@gmail.com>
2022-06-18 17:35:38 +0100

    Added the other scenario for edit todo

--- cypresstests/cypress/integration/TodoSQ/EditTodo.feature
+++ cypresstests/cypress/integration/TodoSQ/EditTodo.feature
@@ -8,9 +8,9 @@ Feature: Edit one todo from the list
         And I change the content of the todo
         Then I see that the todo has changed
 
-#    Scenario: Editing a todo without typing a word
-#        Given I open the main page
-#        And I have todos on the list
-#        When I click on the edit button
-#        And I remove what was written
-#        Then I see an error message
+    Scenario: Editing a todo without typing a word
+        Given I open the main page
+        And I have todos on the list
+        When I click on the edit button
+        And I remove what was written
+        Then I should stay on the same page
--- cypresstests/cypress/integration/TodoSQ/EditTodo/edittodo.js
+++ cypresstests/cypress/integration/TodoSQ/EditTodo/edittodo.js
@@ -13,11 +13,7 @@ And('I have todos on the list', () => {
  cy.get('form > button').click()
  cy.wait(1000)
 })
-/*
-When('I click on the edit button', () => {
- cy.get('li:last-child()').contains(todo).click()
-})
-*/
+
 When('I click on the edit button', () => {
  cy.get('.edit-button:last-child()')
  cy.wait(1000)
@@ -36,8 +32,33 @@ Then('I see that the todo has changed', () => {
     cy.wait(1000)
     cy.get('li').contains(todo)
 })
-/*
-Then('I see that it was deleted from the list', () => {
- cy.get('li:last-child()').not().contains(todo)
+
+
+
+Given('I open the main page', () => {
+ let url = Cypress.config().baseUrl;
+ cy.visit(url)
 })
-*/
+
+And('I have todos on the list', () => {
+ todo = "todo";
+ cy.get('input').type(todo)
+ cy.get('form > button').click()
+ cy.wait(1000)
+})
+
+When('I click on the edit button', () => {
+ cy.get('.edit-button:last-child()')
+ cy.wait(1000)
+ cy.visit(`http://localhost:8080/todo/${todo}/edit`)
+})
+
+And('I remove what was written', () => {
+    cy.get('input').clear()
+    cy.get('form > button').click()
+    cy.wait(1000)
+})
+
+Then('I should stay on the same page', () => {
+    cy.url().should('eq', `http://localhost:8080/todo/${todo}/edit`)
+})
